85018c8a40cab9d36a106d26eefc23108e1a0943
allanornel <allanornel@gmail.com>
2022-07-30 15:14:55 -0300

    feat: unit tests done

--- back-end/tests/unit/recommendationsService.test.ts
+++ back-end/tests/unit/recommendationsService.test.ts
@@ -2,10 +2,11 @@ import { recommendationService, CreateRecommendationData } from "./../../src/ser
 import { recommendationRepository } from "./../../src/repositories/recommendationRepository.js";
 import { jest } from "@jest/globals";
 import { notFoundError } from "../../src/utils/errorUtils.js";
+import { validRecommendation } from "../factories/recommendationFactory.js";
 
 jest.mock("./../../src/services/recommendationsService");
 
-describe("recommendationService test suite", () => {
+describe("Insert Recommendation test suite", () => {
   it("Should creat recommendation", async () => {
     jest.spyOn(recommendationRepository, "findByName").mockImplementationOnce((): any => {});
     jest.spyOn(recommendationRepository, "create").mockImplementationOnce((): any => {});
@@ -25,16 +26,13 @@ describe("recommendationService test suite", () => {
     const promise = recommendationService.insert(recommendation);
     expect(promise).rejects.toEqual({ type: "conflict", message: "Recommendations names must be unique" });
   });
+});
 
+describe("Upvote Recommendation test suite", () => {
   it("Should upvote a recommendation", async () => {
     const id = 1;
     jest.spyOn(recommendationRepository, "find").mockImplementationOnce((): any => {
-      return {
-        id,
-        name: "nome qlqr",
-        youtubeLink: "youtube.com/asdasdas",
-        score: 1,
-      };
+      return { id, ...validRecommendation() };
     });
     jest.spyOn(recommendationRepository, "updateScore").mockImplementationOnce((): any => {});
 
@@ -50,15 +48,11 @@ describe("recommendationService test suite", () => {
     const promise = recommendationService.upvote(id);
     expect(promise).rejects.toEqual(notFoundError());
   });
-
+});
+describe("Downvote Recommendation test suite", () => {
   it("Should downvote a recommendation", async () => {
     const id = 1;
-    const recommendation = {
-      id,
-      name: "nome qlqr",
-      youtubeLink: "youtube.com/asdasdas",
-      score: 1,
-    };
+    const recommendation = { id, ...validRecommendation() };
     jest.spyOn(recommendationRepository, "find").mockImplementationOnce((): any => {
       return recommendation;
     });
@@ -80,12 +74,7 @@ describe("recommendationService test suite", () => {
 
   it("On downvote with -5 score, should delete the recommendation", async () => {
     const id = 1;
-    const recommendation = {
-      id,
-      name: "nome qlqr",
-      youtubeLink: "youtube.com/asdasdas",
-      score: -5,
-    };
+    const recommendation = { id, ...validRecommendation(-5) };
     jest.spyOn(recommendationRepository, "find").mockImplementationOnce((): any => {
       return recommendation;
     });
@@ -97,5 +86,64 @@ describe("recommendationService test suite", () => {
     expect(recommendationRepository.find).toBeCalled();
     expect(recommendationRepository.updateScore).toBeCalled();
     expect(recommendationRepository.remove).toBeCalled();
+  });
+});
+
+describe("Get Recommendations Test Suite", () => {
+  it("Should get a recommendation", async () => {
+    jest.spyOn(recommendationRepository, "findAll").mockImplementationOnce((): any => {});
+    await recommendationService.get();
+    expect(recommendationRepository.find).toBeCalled();
+  });
+
+  it("Should get top recommendations", async () => {
+    jest.spyOn(recommendationRepository, "getAmountByScore").mockImplementationOnce((): any => {});
+    await recommendationService.getTop(10);
+    expect(recommendationRepository.getAmountByScore).toBeCalled();
+  });
+
+  it("Should getRandom recommendation with differents percentages and greater than 10", async () => {
+    jest.spyOn(Math, "random").mockImplementationOnce((): any => {
+      return 0.8;
+    });
+    jest.spyOn(recommendationRepository, "findAll").mockImplementationOnce((): any => {
+      return [
+        { id: 1, ...validRecommendation(50) },
+        { id: 2, ...validRecommendation() },
+        { id: 3, ...validRecommendation() },
+      ];
+    });
+    const recommendation = await recommendationService.getRandom();
+    expect(recommendationRepository.findAll).toHaveBeenCalled();
+    expect(recommendation).not.toBeNull;
+    expect(recommendation).not.toBeUndefined;
+  });
+  it("Should getRandom recommendation with differents percentages and less than or equal to 10", async () => {
+    jest.spyOn(Math, "random").mockImplementationOnce((): any => {
+      return 0.5;
+    });
+    jest.spyOn(recommendationRepository, "findAll").mockImplementationOnce((): any => {
+      return [
+        { id: 1, ...validRecommendation(50) },
+        { id: 2, ...validRecommendation() },
+        { id: 3, ...validRecommendation() },
+      ];
+    });
+    const recommendation = await recommendationService.getRandom();
+    expect(recommendationRepository.findAll).toHaveBeenCalled();
+    expect(recommendation).not.toBeNull;
+    expect(recommendation).not.toBeUndefined;
+  });
+
+  it("Don't have recommendations, should give an error", () => {
+    jest.spyOn(recommendationRepository, "findAll").mockImplementationOnce((): any => {
+      return [];
+    });
+    jest.spyOn(recommendationRepository, "findAll").mockImplementationOnce((): any => {
+      return [];
+    });
+    const promise = recommendationService.getRandom();
+    expect(recommendationRepository.findAll).toHaveBeenCalled();
+    expect(promise).rejects.toEqual(notFoundError());
   });
 });
